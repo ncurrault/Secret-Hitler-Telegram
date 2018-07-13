eb627686912a901425d9d6c3cff6521c27377f98
Nicholas Currault <nicholas.curr+dev@gmail.com>
2018-07-12 19:50:44 -0700

    Revert "webhooks requires SSL"
    This reverts commit 44bcaf7013c8bfaf4514ca8049accfe72a3c61f0.

--- telegram_integration.py
+++ telegram_integration.py
@@ -256,9 +256,11 @@ if __name__ == "__main__":
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO) # not sure exactly how this works
 
-    if os.path.exists("ignore/ssl-key.key") and os.path.exists("ignore/ssl-cert.pem"):
-        updater.start_webhook(url_path=API_KEY)
-    else:
+    try:
+        with open("ignore/webhook.txt") as f:
+            url_path = f.read()
+            updater.start_webhook(url_path=url_path)
+    except IOError:
         updater.start_polling()
 
     updater.idle()
